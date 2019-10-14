e5a63b09f6f622e7c27bdeec1190fa8048636e58
azad71 <mdmamun71@gmail.com>
2019-10-14 15:10:06 +0600

    Updates

---
+++ precache-manifest.1c657d56c8037122fb4306705c056d7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865be4101fc9257bd87c182d2367f91c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2979f8953432765b81a",
+    "url": "/monsters-rolodex/static/css/main.0221b023.chunk.css"
+  },
+  {
+    "revision": "37d17886be92d31610d1",
+    "url": "/monsters-rolodex/static/js/2.d9a63a23.chunk.js"
+  },
+  {
+    "revision": "b2979f8953432765b81a",
+    "url": "/monsters-rolodex/static/js/main.85654b62.chunk.js"
+  },
+  {
+    "revision": "6de4edb3edbf96ca849b",
+    "url": "/monsters-rolodex/static/js/runtime~main.b5d8705c.js"
+  }
+]);
